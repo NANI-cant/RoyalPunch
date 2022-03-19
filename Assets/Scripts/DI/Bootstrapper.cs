97fdb61da7cacbd85837e540ce28504f04cde969
Дмитрий Чмелёв <trixxx.asd@yandex.ru>
2022-03-19 22:20:45 +0300

    Re foldering

--- Assets/Scripts/DI/Bootstrapper.cs
+++ Assets/Scripts/DI/Bootstrapper.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 using Zenject;
 
 public class Bootstrapper : MonoInstaller {
+    [Header("UI")]
     [SerializeField] private Joystick _joystick;
+    [SerializeField] private StartPanel _startPanel;
+
+    [Header("Gameplay")]
     [SerializeField] private Player _player;
     [SerializeField] private Enemy _enemy;
-    [SerializeField] private StartPanel _startPanel;
+
+    [Header("Global")]
     [SerializeField] private Camera _mainCamera;
 
     public override void InstallBindings() {
